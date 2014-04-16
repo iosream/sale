aa10826eedb47288393485325fe87ad2cb654752
wangzhi <286233579@qq.com>
2014-04-16 09:08:39 +0800

    to be continued

--- src/main/java/com/sale/controller/BaseController.java
+++ src/main/java/com/sale/controller/BaseController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController {
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String base(Model model) {
         model.addAttribute("message", "Success");
         return "index";
